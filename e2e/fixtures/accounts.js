1704cd68e9b4950ca2f5142e3f2726da6d0c6cff
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-09-08 01:30:28 -0500

    test(account): make accounts fixture more complete

--- e2e/fixtures/accounts.js
+++ e2e/fixtures/accounts.js
@@ -2,11 +2,38 @@ export const accounts = {
   data: {
     accounts: [
       {
-        name: 'account1',
+        name: 'openAndOnBudget',
         balance: 0,
         cleared_balance: 0,
-        uncleared_balance: 5
+        uncleared_balance: 5,
+        closed: false,
+        on_budget: true
+      },
+      {
+        name: 'closedAndOnBudget',
+        balance: 100,
+        cleared_balance: 0,
+        uncleared_balance: 5,
+        closed: true,
+        on_budget: true
+      },
+      {
+        name: 'openAndOffBudget',
+        balance: 200,
+        cleared_balance: 0,
+        uncleared_balance: 5,
+        closed: false,
+        on_budget: false
+      },
+      {
+        name: 'closedAndOffBudget',
+        balance: 300,
+        cleared_balance: 0,
+        uncleared_balance: 5,
+        closed: true,
+        on_budget: false
       }
+
     ]
   }
 }
